6724428178c6f0a3980fc4c8f48900909b2ed684
Mateo Roizman <carlotaroizman@gmail.com>
2023-08-10 16:35:25 -0300

    Login, register, and reset password

---
+++ website/public/Sources/js/LoginPage.js
@@ -0,0 +1,17 @@
+function showLoginForm() {
+	document.getElementById("login-form").hidden = false;
+	document.getElementById("register-form").hidden = true;
+	document.getElementById("password-reset-form").hidden = true;
+}
+
+function showRegisterForm() {
+	document.getElementById("login-form").hidden = true;
+	document.getElementById("register-form").hidden = false;
+	document.getElementById("password-reset-form").hidden = true;
+}
+
+function showPasswordResetForm() {
+	document.getElementById("login-form").hidden = true;
+	document.getElementById("register-form").hidden = true;
+	document.getElementById("password-reset-form").hidden = false;
+}
